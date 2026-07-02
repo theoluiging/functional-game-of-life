8fa857465fe55d85718a274e08045715e68dad1c
theoluiging <theoluiging@gmail.com>
2026-07-02 15:18:31 -0300

    Estados calculados automaticamente em sequência

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,22 @@
 module Main where
 
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever)
+
 import Brick
+import Brick.BChan (newBChan, writeBChan)
+
 import Lens.Micro.Mtl ( (%=), use, (.=) )
-import Graphics.Vty as Vty (
-    Event(EvKey), 
-    Key(KChar, KEsc, KUp, KDown, KLeft, KRight, KEnter), 
-    defAttr) 
+
+import qualified Graphics.Vty as Vty
+import qualified Graphics.Vty.CrossPlatform as VCP
+
 import Mechanics
 import UI (drawUI)
 import Logic ( nextStep )
+
+
+data Tick = Tick
 
 handleMoveEvent :: Mechanics.Direction -> EventM () GameState ()
 handleMoveEvent dir = do
@@ -21,7 +29,12 @@ handleNextState = do
     modify limitCells
 
 -- Recebe o evento atual e decide o que fazer com o Estado na Mónada de Eventos.
-handleEvent :: BrickEvent () e -> EventM () GameState ()
+handleEvent :: BrickEvent () Tick -> EventM () GameState ()
+handleEvent (AppEvent Tick) = do
+    pausado <- use gamePaused
+    if pausado
+        then return () -- Se estiver pausado, não faz nada
+        else do handleNextState
 handleEvent (VtyEvent e) = case e of
     Vty.EvKey (Vty.KChar 'q') [] -> halt
     Vty.EvKey Vty.KEsc        [] -> halt
@@ -35,26 +48,39 @@ handleEvent (VtyEvent e) = case e of
     Vty.EvKey (Vty.KChar 'd') [] -> handleMoveEvent R
     Vty.EvKey (Vty.KChar 'r') [] -> do 
         liveCells .= []
-        gamePaused %= not
+        gamePaused .= True
     Vty.EvKey (Vty.KChar ' ') [] -> do
         c <- use cursorPos
         liveCells %= toggleCell c
     Vty.EvKey (Vty.KChar 't') [] -> handleNextState
     Vty.EvKey Vty.KEnter      [] -> gamePaused %= not
-    _ -> return ()
-handleEvent _ = return () -- Para qualquer outra tecla, não faz nada
+    _ -> return () -- Para qualquer outra tecla, não faz nada
+handleEvent _ = return () 
 
 
-app :: App GameState e ()
+app :: App GameState Tick ()
 app = App { appDraw         = \st -> [drawUI st]
           , appChooseCursor = neverShowCursor
           , appHandleEvent  = handleEvent
           , appStartEvent   = return ()
-          , appAttrMap      = const $ attrMap defAttr []
+          , appAttrMap      = const $ attrMap Vty.defAttr []
           }
 
 
 main :: IO ()
 main = do
-    _ <- defaultMain app initialState
+    -- canal de comunicação do Brick que suporta até 10 mensagens na fila
+    chan <- newBChan 10
+
+    -- Metronomo em thread paralela
+    _ <- forkIO $ forever $ do
+        writeBChan chan Tick
+        threadDelay 500000     -- 500,000 microssegundos (0.5 segundos)
+
+    -- Configuração do customMain
+    let buildVty = VCP.mkVty Vty.defaultConfig
+    initialVty <- buildVty
+
+    _ <- customMain initialVty buildVty (Just chan) app initialState
+    
     return ()
